596fe81378b7a61b284a99fb750b914339b08576
Jeff Campbell <jeff@zapdot.com>
2020-10-09 08:35:37 -0400

    v1.3.2
    ## Summary
    This release contains a small quality of life addition for refactor mode. When refactor mode is enabled and a developer attempts to enter play mode, a modal warning will be shown that it should be disabled before playing. If **Disable Refactor Mode** is selected, play mode will be stopped and refactor mode disabled, which will cause a script recompile. If **Enter Playmode** is selected, playmode will proceed and a warning issued in the console.
    
    **Note:** If you install EntitasRedux using a `.UnityPackage` you must also grab the latest release of Genesis as well from [here](https://github.com/jeffcampbellmakesgames/Genesis) as it is not included. For better dependency handling, I would recommend using OpenUPM instead.
    
    ## ADDED
    * Added a warning modal dialog to be shown when when entering playmode and refactor mode is enabled.
    
    ## CHANGED
    * Refactored some internals for refactor mode for easy reuse elsewhere in the framework.
    
    ## FIXED
    * NA
    
    ## REMOVED
    * NA

---
+++ Scripts/Editor/PlayModeHelper.cs
@@ -0,0 +1,50 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace JCMG.EntitasRedux.Editor
+{
+	/// <summary>
+	/// A helper class for resolving general play-mode issues.
+	/// </summary>
+	[InitializeOnLoad]
+	public static class PlayModeHelper
+	{
+		// Refactor Mode
+		private const string REFACTOR_MODE_TITLE = "[EntitasRedux] Refactor Mode Enabled!";
+		private const string REFACTOR_MODE_MSG
+			= "Please disable Refactor Mode before playing in the Editor. This can result in errors due " +
+			  "to parts of EntitasRedux being compiled out.";
+		private const string REFACTOR_MODE_OK_TEXT = "Disable Refactor Mode";
+		private const string REFACTOR_MODE_CANCEL_TEXT = "Enter PlayMode";
+
+		private const string REFACTOR_MODE_MSG_WARNING = "[EntitasRedux] " + REFACTOR_MODE_MSG;
+
+		static PlayModeHelper()
+		{
+			EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
+		}
+
+		private static void OnPlayModeStateChanged(PlayModeStateChange playModeState)
+		{
+			if (RefactorModeTools.IsRefactorModeEnabled() &&
+			    playModeState == PlayModeStateChange.ExitingEditMode)
+			{
+				var disableRefactorMode = EditorUtility.DisplayDialog(
+					title: REFACTOR_MODE_TITLE,
+					message: REFACTOR_MODE_MSG,
+					ok: REFACTOR_MODE_OK_TEXT,
+					cancel: REFACTOR_MODE_CANCEL_TEXT);
+
+				if (disableRefactorMode)
+				{
+					EditorApplication.ExitPlaymode();
+					RefactorModeTools.DisableRefactorMode();
+				}
+				else
+				{
+					Debug.LogWarning(REFACTOR_MODE_MSG_WARNING);
+				}
+			}
+		}
+	}
+}
---
+++ Scripts/Editor/Tools/RefactorModeTools.cs
@@ -0,0 +1,60 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace JCMG.EntitasRedux.Editor
+{
+	/// <summary>
+	/// Helper methods for refactor mode
+	/// </summary>
+	internal static class RefactorModeTools
+	{
+		private const string SCRIPTING_SYMBOL = "ENTITAS_REDUX_NO_IMPL";
+		private const string ENABLED_REFACTOR_MODE_MSG = "[EntitasRedux] Enabled refactor mode...";
+		private const string DISABLED_REFACTOR_MODE_MSG = "[EntitasRedux] Disabled refactor mode...";
+
+		/// <summary>
+		/// Returns true if refactor mode is enabled, otherwise false.
+		/// </summary>
+		public static bool IsRefactorModeEnabled()
+		{
+			#if ENTITAS_REDUX_NO_IMPL
+			return true;
+			#else
+			return false;
+			#endif
+		}
+
+		/// <summary>
+		/// Enables refactor mode by adding a unique scripting symbol `ENTITAS_REDUX_NO_IMPL` to
+		/// <see cref="PlayerSettings"/> if not already present.
+		/// </summary>
+		public static void EnableRefactorMode()
+		{
+			if (EditorApplication.isCompiling)
+			{
+				return;
+			}
+
+			PlayerSettingsTools.AddScriptingSymbol(SCRIPTING_SYMBOL);
+
+			Debug.Log(ENABLED_REFACTOR_MODE_MSG);
+
+		}
+
+		/// <summary>
+		/// Enables refactor mode by removing a unique scripting symbol `ENTITAS_REDUX_NO_IMPL` from
+		/// <see cref="PlayerSettings"/> if present.
+		/// </summary>
+		internal static void DisableRefactorMode()
+		{
+			if (EditorApplication.isCompiling)
+			{
+				return;
+			}
+
+			PlayerSettingsTools.RemoveScriptingSymbol(SCRIPTING_SYMBOL);
+
+			Debug.Log(DISABLED_REFACTOR_MODE_MSG);
+		}
+	}
+}
